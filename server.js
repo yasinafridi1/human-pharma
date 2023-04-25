203a3484d1b0bf831a87b0d7c25f79782f52dc06
yasinafridi11 <khialyyasin@gmail.com>
2023-04-26 00:51:21 +0500

    last build

--- server.js
+++ server.js
@@ -12,19 +12,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api', Router);
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (req, res) {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        function (err) {
-            res.status(500).send(err);
-        }
-    )
-})
-const port = process.env.PORT || 8000
 
-mongoose.connect(process.env.MONGO_URL || "mongodb://humansteriodspharma:abc7777578@ac-olmdg0d-shard-00-00.hgrkylq.mongodb.net:27017,ac-olmdg0d-shard-00-01.hgrkylq.mongodb.net:27017,ac-olmdg0d-shard-00-02.hgrkylq.mongodb.net:27017/humansteroid?replicaSet=atlas-12oyil-shard-0&ssl=true&authSource=admin", {
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(() => {
     console.log('Welcome to Human Pharma Steriods');
@@ -37,4 +28,13 @@ mongoose.connect(process.env.MONGO_URL || "mongodb://humansteriodspharma:abc7777
 })
 
 
-
+app.use('/api', Router);
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "./frontend/build/index.html"),
+        function (err) {
+            res.status(500).send(err);
+        }
+    )
+})
